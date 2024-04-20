775ede1be3fa0d66b7c2ff2109a73cb0af23f9b4
Евгений Шумаков <daredevil1984@list.ru>
2024-04-20 10:42:58 +0500

    Добавьте файлы проекта.

---
+++ Geometry/Program.cs
@@ -0,0 +1,17 @@
+﻿using GeometricShapesLibrary; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double squareArea = GeometricShapes.SquareArea(5);
+        double rectangleArea = GeometricShapes.RectangleArea(4, 6);
+        double triangleArea = GeometricShapes.TriangleArea(3, 4);
+
+        Console.WriteLine("Square area: " + squareArea);
+        Console.WriteLine("Rectangle area: " + rectangleArea);
+        Console.WriteLine("Triangle area: " + triangleArea);
+
+        Console.ReadLine();
+    }
+}
---
+++ TextUtils/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TextUtilsLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "А роза упала на лапу Азора";
+
+        Console.WriteLine($"Строка \"{text}\" является палиндромом: {text.IsPalindrome()}");
+        Console.WriteLine($"Количество слов в строке: {text.WordCount()}");
+        Console.WriteLine($"Количество предложений в строке: {text.SentenceCount()}");
+        Console.WriteLine($"Количество символов в строке: {text.CharacterCount()}");
+
+        Console.ReadLine();
+    }
+}
